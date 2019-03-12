c903bf1b157b35c7e657c758b6224645185f5cde
nurlanf <nurlan.farajov@gmail.com>
2019-03-12 12:29:09 +0100

    Update

--- bootstrap_all.sh
+++ bootstrap_all.sh
@@ -25,10 +25,4 @@ deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 apt update
 apt install -y kubelet kubeadm kubectl
-sudo kubeadm init --pod-network-cidr=192.168.0.0/16
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
-kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
 
---
+++ bootstrap_master.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+
