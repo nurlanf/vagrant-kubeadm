29d1fef3d1d4c465f80c1496aab9043b6a668d09
nurlanf <nurlan.farajov@gmail.com>
2019-03-12 12:57:29 +0100

    Update

--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
-sudo kubeadm init --pod-network-cidr=192.168.0.0/16
+sed -i '0,/ExecStart=/s//Environment="KUBELET_EXTRA_ARGS=--cgroup-driver=cgroupfs"\\n&/' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+# Get the IP address that VirtualBox has given this VM
+IPADDR=`ifconfig eth1 | grep -i Mask | awk '{print $2}'| cut -f2 -d:`
+echo This VM has IP address $IPADDR
+# Set up Kubernetes
+NODENAME=$(hostname -s)
+kubeadm init --apiserver-cert-extra-sans=$IPADDR  --node-name $NODENAME
 sudo --user=vagrant mkdir -p /home/vagrant/.kube
-sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
-sudo chown $(id -u vagrant):$(id -g vagrant) /home/vagrant/.kube/config
-sudo kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
-sudo kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+chown $(id -u vagrant):$(id -g vagrant) /home/vagrant/.kube/config
+kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
 
