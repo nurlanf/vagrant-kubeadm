5431874b0960672f46da1b987e92e9bc27c650ca
nurlanf <nurlan.farajov@gmail.com>
2019-03-12 16:40:18 +0100

    Update

--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-kubeadm init --pod-network-cidr=192.168.0.0/16 --token qnq2lo.v0acaocjegmzf06c --node-name kube-master
+kubeadm init --apiserver-advertise-address=192.168.50.4 --pod-network-cidr=192.168.0.0/16 --token qnq2lo.v0acaocjegmzf06c
 sudo --user=vagrant mkdir -p /home/vagrant/.kube
 cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
 chown $(id -u vagrant):$(id -g vagrant) /home/vagrant/.kube/config
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
 kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
 
--- bootstrap_node.sh
+++ bootstrap_node.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-NODENAME=$(hostname -s)
-kubeadm join 192.168.50.4:6443 --token qnq2lo.v0acaocjegmzf06c --discovery-token-unsafe-skip-ca-verification --node-name kube-master
+kubeadm join 192.168.50.4:6443 --token qnq2lo.v0acaocjegmzf06c --discovery-token-unsafe-skip-ca-verification
