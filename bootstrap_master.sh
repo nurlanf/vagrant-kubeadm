a61bce0fa5f5c1ef4c3a95aef41656bfbf465b4a
nurlanf <nurlan.farajov@gmail.com>
2019-03-18 12:59:25 +0100

    Update

--- bootstrap_master.sh
+++ bootstrap_master.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # DON'T CONFIGURE THIS FILE
-kubeadm init --apiserver-advertise-address=MASTER_IPADDR --pod-network-cidr=POD_CIDR --token TOKEN --token-ttl TOKEN-TTL
+kubeadm init --apiserver-advertise-address=MASTER_IPADDR --pod-network-cidr=POD_CIDR --token TOKEN --token-ttl TOKENTTL
 sudo --user=vagrant mkdir -p /home/vagrant/.kube
 cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
 chown $(id -u vagrant):$(id -g vagrant) /home/vagrant/.kube/config
