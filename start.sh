17a22ae44b95a7db7abff3134611f179c37141a7
nurlanf <nurlan.farajov@gmail.com>
2019-03-20 14:52:34 +0100

    Update

--- start.sh
+++ start.sh
@@ -53,6 +53,7 @@ cat << EOF >> bootstrap_master.sh
 kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
 kubectl apply -f https://docs.projectcalico.org/v3.2/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
 EOF
+fi
 sleep 1
 
 if [[ $CNI == flannel ]];
@@ -60,6 +61,7 @@ then
 cat << EOF >> bootstrap_master.sh
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 EOF
+fi
 sleep 1
 
 
